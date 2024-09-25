1d18fef55d4c64a9b7cc62f0dd1ba5a02a361f4c
asinha26 <aaryaksinha@gmail.com>
2024-09-25 18:13:31 -0400

    fixed location again hopefully fr this time

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,19 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import UserProfile, FavoriteRestaurant
 
 # Define an inline admin descriptor for UserProfile model
-# This allows us to edit UserProfile fields in the User admin interface
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     verbose_name_plural = 'Profile'
     fk_name = 'user'
 
-# Extend the existing UserAdmin to include UserProfile
+# Define an inline admin descriptor for FavoriteRestaurant model
+class FavoriteRestaurantInline(admin.TabularInline):
+    model = FavoriteRestaurant
+    can_delete = True
+    extra = 0  # Number of extra blank fields for new entries
+    verbose_name_plural = 'Favorite Restaurants'
+    fk_name = 'user'
+
+# Extend the existing UserAdmin to include UserProfile and FavoriteRestaurant inlines
 class UserAdmin(BaseUserAdmin):
-    inlines = (UserProfileInline,)
+    inlines = (UserProfileInline, FavoriteRestaurantInline)
 
     # Customize list display to show extra fields in the user list in the admin panel
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'get_favorite_cuisine')
@@ -26,3 +33,4 @@ class UserAdmin(BaseUserAdmin):
 # Unregister the default User admin and register the customized one
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
